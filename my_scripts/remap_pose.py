3cb7e05d0990bc3fe0504483ee0cf9c954b7dc67
YassineMch <yassine.mechri@stud.uni-hannover.de>
2022-07-13 17:01:13 +0200

    new node

--- my_scripts/remap_pose.py
+++ my_scripts/remap_pose.py
@@ -4,35 +4,43 @@ import rospy
 from marvelmind_nav.msg import hedge_pos_ang
 from geometry_msgs.msg import PoseStamped
 
-seq_init = 0;
-pose_out = PoseStamped()
+seq_init = 0 
 
-def marvelmind_pos_callback(msg_pos):
-    global seq_init
+
+def callback_marvelmind_pos(msg_pos):
+    
+    pose_out = PoseStamped()
+    
     
 # define header 
-pose_out.header.stamp = rospy.Time.now()
-pose_out.header.frame_id = 'odom'
+    global seq_init
 
-pose_out.header.seq = seq_init
-seq_init += 1
+    pose_out.header.seq = seq_init
+    seq_init += 1
+
+    pose_out.header.stamp = rospy.Time.now()
+    pose_out.header.frame_id = 'odom'
+
+
  
 # position
-pose_out.pose.position.x = msg_pos.x_m
-pose_out.pose.position.y = msg_pos.y_m
-pose_out.pose.position.y = msg_pos.z_m
+    pose_out.pose.position.x = msg_pos.x_m
+    pose_out.pose.position.y = msg_pos.y_m
+    pose_out.pose.position.y = msg_pos.z_m
 
 #Quaterion 
-pose_out.pose.orientation.x = 0
-pose_out.pose.orientation.y = 0
-pose_out.pose.orientation.z = 0
-pose_out.pose.orientation.w = 0
+    pose_out.pose.orientation.x = 0
+    pose_out.pose.orientation.y = 0
+    pose_out.pose.orientation.z = 0
+    pose_out.pose.orientation.w = 0
+    
+    pub.publish(pose_out)
 
 if __name__ == '__main__':
     
-    rospy.init_node('position_marvelmind')
+    rospy.init_node('remap_pose')
     
-    pose_out_publisher = rospy.Publisher("/pose_out", PoseStamped, queue_size=5)
-    pose_in_Suscriber = rospy.Subscriber("/hedge_pos_ang", msg_pos, marvelmind_pos_callback )
+    sub = rospy.Subscriber("hedge1/hedge_pos_ang", hedge_pos_ang, callback_marvelmind_pos)
+    pub = rospy.Publisher("/pose_marvelmind", PoseStamped, queue_size=10)
     
     rospy.spin()

de86f43e24ad3a3a5e60fa4bc6aab7a19d443161
YassineMch <yassine.mechri@stud.uni-hannover.de>
2022-07-21 21:09:35 +0200

    commit

--- my_scripts/changepose.py
+++ my_scripts/changepose.py
@@ -1,37 +1,36 @@
-#!/usr/bin/env python3
-
+#! /usr/bin/env python3
 import rospy
-from geometry_msgs.msg import PoseWithCovarianceStamped
-from geometry_msgs.msg import PoseStamped
 
-t_n = 0 
+from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
 
+prev_time = 0
 
-def callback_marvelmind_pos(msg_pos):
-    global t_n
-    t_n1 = rospy.Time.now
-    updateTime = 5
-    pose_out = PoseWithCovarianceStamped()
+def callback_send_poseestimate(msg_in):
     
+    #timer to limit publishing rate
+    global prev_time
+    interval = 4 #seconds
+    currentTime = rospy.get_time()
     
-# define header 
+    #type definition
+    msg_out = PoseWithCovarianceStamped()
 
-    pose_out.header = msg_pos.header
-    pose_out.pose.pose = msg_pos.pose
+    #transfer header and pose from original message
+    msg_out.header = msg_in.header
+    msg_out.pose.pose = msg_in.pose
 
-
-# covariance
-    pose_out.covariance = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-
+    #create covariance matrix, empty for now
+    msg_out.pose.covariance = [0.25,0,0,0,0,0,0,0.25,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.06853892326654787]
     
-    pub.publish(pose_out)
-    
-    if (t_n1-t_n > updateTime):
-        pub.publish(pose_out)
-        t_n = rospy.get_time()
-        
-if __name__ =='__main__':
-    rospy.init_node('changepose')
-    sub = rospy.Subscriber("/position_marvelmind", PoseStamped, callback_marvelmind_pos)
+    if (currentTime-prev_time > interval):
+        #publish converted message
+        pub.publish(msg_out)
+        prev_time = rospy.get_time()
+
+
+if __name__=='__main__':
+    rospy.init_node("changepose")
+    sub = rospy.Subscriber("/position_marvelmind", PoseStamped, callback_send_poseestimate)
     pub = rospy.Publisher("/initialpose", PoseWithCovarianceStamped, queue_size=10)
-    rospy.spin
+    rospy.spin()
+    

a18c5d5b67eba2597eda19f35548b7738399c300
YassineMch <yassine.mechri@stud.uni-hannover.de>
2022-07-22 15:27:26 +0200

    commit

--- my_scripts/src/changepose.py
+++ my_scripts/src/changepose.py
@@ -1,13 +1,12 @@
 #! /usr/bin/env python3
 import rospy
-
-from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
-
+from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import PoseStamped
 t_n = 0
 
 def callback_position(msg_in):
     global t_n
-    update_rate = 4 
+    update_rate = 10 
     rospy.get_time()
     
     #transfer Data to PoseWithCovariancestamped
@@ -15,14 +14,12 @@ def callback_position(msg_in):
     initpose_msg.header = msg_in.header
     initpose_msg.pose.pose = msg_in.pose
 
-    #covariance matrix
-    initpose_msg.pose.covariance = [0.0004, 0, 0, 0, 0, 0, 0, 0.0004, 0, 0, 0, 0, 0, 0, 0.0004, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.0004]
+    #covariance matrix mit der Variance von den Punkten bestimmt
+    initpose_msg.pose.covariance = [0.007376561813186813, 0, 0, 0, 0, 0, 0, 0.007376561813186813, 0, 0, 0, 0, 0, 0, 0.007376561813186813, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1.865003690775747e-06]
     
     if (rospy.get_time()-t_n > update_rate):
-
         pub.publish(initpose_msg)
         t_n = rospy.get_time()
-
 
 if __name__=='__main__':
     
---
+++ my_scripts/src/define_variance.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+
+import rospy
+import time
+import statistics
+from geometry_msgs.msg import PoseStamped
+#import numpy as np
+
+x = []
+y = []
+z = []
+yaw = []
+x_1 = 0
+y_1= 0
+yaw_1= 0
+angle = 0
+
+def callback_marvelmind_pos(msg):   
+    global x, y, z, yaw, x_1, y_1, z_1, angle
+    
+    x_1 = msg.pose.position.x
+    y_1 = msg.pose.position.y
+    z_1 = msg.pose.position.z
+    
+    x.append(msg.pose.position.x)
+    y.append(msg.pose.position.y)
+    z.append(msg.pose.position.z)
+    yaw.append(msg.pose.orientation.w)
+    
+    
+    
+if __name__ =='__main__':
+    rospy.init_node('define_variance')
+    sub=rospy.Subscriber("/position_marvelmind", PoseStamped, callback_marvelmind_pos)
+    
+    time.sleep(10)
+    
+    variance_x = statistics.variance(x)
+    variance_y = statistics.variance(y)
+    variance_z = statistics.variance(z)
+    variance_yaw = statistics.variance (yaw)
+    
+    print('variance x =', variance_x, 'variance y =', variance_y,  'variance z =', variance_z, 'variance yaw =', variance_yaw)
+    

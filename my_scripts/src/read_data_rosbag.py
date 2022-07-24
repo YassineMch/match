f56bb0008176510b35cb308b5d1e77a3a053163f
YassineMch <yassine.mechri@stud.uni-hannover.de>
2022-07-24 23:50:11 +0200

    fuck i am tired

--- my_scripts/src/read_data_rosbag.py
+++ my_scripts/src/read_data_rosbag.py
@@ -3,6 +3,8 @@
 #import rospy
 import rosbag
 import statistics
+import numpy as np
+from array import array
 #from geometry_msgs.msg import PoseStamped
 
 x = []
@@ -12,6 +14,7 @@ yaw = []
 
 bag = rosbag.Bag ('/home/ros/catkin_ws_yassin/src/match/my_scripts/recording/30_sec_stationairy.bag')
 
+# open the .bag file
 
 for topic, msg, t in bag.read_messages(topics=['/position_marvelmind']):
 
@@ -22,9 +25,26 @@ for topic, msg, t in bag.read_messages(topics=['/position_marvelmind']):
 
 bag.close()
 
+x = np.array ([x])
+y = np.array ([y])
+z = np.array ([z])
+yaw = np.array ([yaw])
+
+#save data in an csv file in /recording/myresults
+#change the name of the file if required
+
+np.savetxt('/home/ros/catkin_ws_yassin/src/match/my_scripts/recording/myresults/x_30_sec_stationary.csv',x, delimiter=",", header="position variation in x direction")
+np.savetxt('/home/ros/catkin_ws_yassin/src/match/my_scripts/recording/myresults/y_30_sec_stationary.csv',y, delimiter=",", header="position variation in y direction")
+np.savetxt('/home/ros/catkin_ws_yassin/src/match/my_scripts/recording/myresults/z_30_sec_stationary.csv',z, delimiter=",", header="position variation in z direction")
+np.savetxt('/home/ros/catkin_ws_yassin/src/match/my_scripts/recording/myresults/yaw_30_sec_stationary.csv',yaw, delimiter=",", header="orientation variation in yaw direction")
+
 # in case to print an array
 
 print ('position variation in x direction:', x, 'position variation in y direction:', y, 'position variation in z direction:', z, 'orientation variation in yaw direction:', yaw)
+
+#print (x)
+
+#in case to define the variance uncomment
 
 #variance_x = statistics.variance(x)
 #variance_y = statistics.variance(y)

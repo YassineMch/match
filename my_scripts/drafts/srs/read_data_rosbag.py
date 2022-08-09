b4c46bfea665d7e72ac4efec73cd89fdeb684428
YassineMch <yassine.mechri@stud.uni-hannover.de>
2022-08-09 08:46:31 +0200

    new name

--- my_scripts/drafts/srs/read_data_rosbag.py
+++ my_scripts/drafts/srs/read_data_rosbag.py
@@ -12,31 +12,32 @@ y = []
 z = []
 yaw = []
 
-bag = rosbag.Bag ('/home/ros/catkin_ws_yassin/src/match/my_scripts/recording/30_sec_stationairy.bag')
+bag = rosbag.Bag ('/home/ros/catkin_ws_yassin/src/match/my_work/aufnahmen/aufnahme_0.2_ms/2022-08-05-13-13-14.bag')
 
 # open the .bag file
 
-for topic, msg, t in bag.read_messages(topics=['/position_marvelmind']):
+for topic, msg, t in bag.read_messages(topics=['/odom']):
 
-    x.append(msg.pose.position.x)
-    y.append(msg.pose.position.y)
-    z.append(msg.pose.position.z)   
-    yaw.append(msg.pose.orientation.z)
+    x.append(msg.pose.pose.position.x)
+    y.append(msg.pose.pose.position.y)
+    z.append(msg.pose.pose.position.z)   
+    #yaw.append(msg.pose.pose.orientation.z)
 
 bag.close()
 
 x = np.array ([x])
 y = np.array ([y])
 z = np.array ([z])
-yaw = np.array ([yaw])
+#yaw = np.array ([yaw])
 
 #save data in an csv file in /recording/myresults
 #change the name of the file if required
 
-np.savetxt('/home/ros/catkin_ws_yassin/src/match/my_scripts/recording/myresults/x_30_sec_stationary.csv',x, delimiter=",", header="position variation in x direction")
-np.savetxt('/home/ros/catkin_ws_yassin/src/match/my_scripts/recording/myresults/y_30_sec_stationary.csv',y, delimiter=",", header="position variation in y direction")
-np.savetxt('/home/ros/catkin_ws_yassin/src/match/my_scripts/recording/myresults/z_30_sec_stationary.csv',z, delimiter=",", header="position variation in z direction")
-np.savetxt('/home/ros/catkin_ws_yassin/src/match/my_scripts/recording/myresults/yaw_30_sec_stationary.csv',yaw, delimiter=",", header="orientation variation in yaw direction")
+
+np.savetxt('/home/ros/catkin_ws_yassin/src/match/aufnahmen/2022-08-05-13-48-37_x_odom.csv',x, delimiter=",", header="position in x direction for 1.6 m/s odom")
+np.savetxt('/home/ros/catkin_ws_yassin/src/match/aufnahmen/2022-08-05-13-48-37_y_odom.csv',y, delimiter=",", header="position in y direction for 1.6 m/s odom")
+np.savetxt('/home/ros/catkin_ws_yassin/src/match/aufnahmen/2022-08-05-13-48-37_z_odom.csv',z, delimiter=",", header="position in z direction for 1.6 m/s odom")
+#np.savetxt('/home/ros/catkin_ws_yassin/src/match/my_scripts/recording/myresults/yaw_30_sec_stationary.csv',yaw, delimiter=",", header="orientation variation in yaw direction")
 
 # in case to print an array
 
